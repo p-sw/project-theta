59f6bcffc1989a318ffbc8ff4de566c7dfc5ab6d
Shinwoo PARK <shinwoo.park@psw.kr>
2025-07-08 01:48:21 +0900

    feat: apply platform-fastify

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+import { ConsoleLogger } from 'nestlogged-fastify';
+
+import { AppModule } from './app.module';
 import { Config } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {
+      logger: new ConsoleLogger(),
+    },
+  );
   const config = app.get(ConfigService<Config>);
   await app.listen(config.getOrThrow('BACKEND_PORT', { infer: true }));
 }
