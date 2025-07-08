f1910c5c2cfe37cdd2e48e78ece347b2022ff085
Shinwoo PARK <shinwoo.park@psw.kr>
2025-07-08 17:09:45 +0900

    refactor: disable '@typescript-eslint/no-namespace' rule in eslint configuration

--- apps/backend/eslint.config.mjs
+++ apps/backend/eslint.config.mjs
@@ -30,6 +30,7 @@ export default tseslint.config(
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-namespace': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       '@typescript-eslint/no-unused-vars': [
