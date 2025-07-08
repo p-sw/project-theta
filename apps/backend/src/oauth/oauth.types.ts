87b4a36165d4e4d7d6a6dd41fcc9460b364f2e18
Shinwoo PARK <shinwoo.park@psw.kr>
2025-07-08 18:46:11 +0900

    feat: add shared types

--- apps/backend/src/oauth/oauth.types.ts
+++ apps/backend/src/oauth/oauth.types.ts
@@ -1,5 +1,20 @@
+import {
+  DiscordAccessTokenReturn,
+  DiscordUserInfoResponse,
+} from './providers/discord.types';
+import {
+  GithubAccessTokenCodeReturn,
+  GithubUserInfoResponse,
+} from './providers/github.types';
+
 export type OAuthProvider = 'github' | 'discord';
 
 export interface OAuthRedirectUriParams {
   code: string;
 }
+
+export type AccessTokenReturn =
+  | DiscordAccessTokenReturn
+  | GithubAccessTokenCodeReturn;
+
+export type UserInfoResponse = DiscordUserInfoResponse | GithubUserInfoResponse;
