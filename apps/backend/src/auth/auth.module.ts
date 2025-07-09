c1c6e3b6fcda449180af4ec0d54b2f36a0d8020a
Shinwoo PARK <shinwoo.park@psw.kr>
2025-07-09 18:54:45 +0900

    fix: add missing OAuthModule to AuthModule

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { OAuthModule } from '@/oauth/oauth.module';
+
 import { AuthController } from './auth.controller';
 import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [OAuthModule],
   controllers: [AuthController],
   providers: [AuthService, AuthGuard],
   exports: [AuthService],
